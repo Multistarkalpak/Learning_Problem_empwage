81c03ed7ca11e8d2b8fa71648b6f2bc6836af6ff
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-22 21:30:22 +0530

    Adding the company

--- Emp_wage/Emp_wage/Program.cs
+++ Emp_wage/Emp_wage/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EMPLOYEEWAGEPROBLEM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1 For TATA Company Employee Wage");
+            Console.WriteLine("2 For MAHINDRA Company Employee Wage");
+
+            int problem = Convert.ToInt32(Console.ReadLine());
+            switch (problem)
+            {
+                case 1:
+                    EmployeeWageComputation employeewagecomputation = new EmployeeWageComputation();
+                    employeewagecomputation.AddCompany("TATA", 20, 8, 4, 100, 20);
+                    employeewagecomputation.CalculateEmpWage("TATA");
+                    break;
+                case 2:
+                    EmployeeWageComputation wagecomputation = new EmployeeWageComputation();
+                    wagecomputation.AddCompany("MAHINDRA", 30, 8, 4, 100, 20);
+                    wagecomputation.CalculateEmpWage("MAHINDRA");
+                    break;
+            }
+        }
+    }
+}
